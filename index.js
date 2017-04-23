01e68b238a509188ebd9aaa46e738b2423cd6050
David Weinstein <dwein9@gmail.com>
2017-04-23 13:00:06 -0400

    mvp

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
+$(document).ready(getHomeRuns)
 
+
+function getHomeRuns() {
+  $('.btn').click(function() {
+    homerCheck()
+  })
+}
+
+function homerCheck() {
   const URL = 'http://www.baseball-reference.com/players/gl.fcgi?id=harpebr03&t=b&year=2017'
 
   $.ajax({
@@ -9,8 +17,21 @@ $(document).ready(function(){
     const table =  $(html).find('table')[4].children[3].innerHTML.split('\n')
     var bryce = table[table.length-3].split('data-stat=')[16].split('>')[1][0]
 
-    console.log(bryce);
+    render(bryce)
     }
 
   })
-})
+}
+
+
+function render(data) {
+  let $answer = $('#answer')
+
+  if (data > 0) {
+    var response = "Yes"
+  } else {
+    var response = "No"
+  }
+
+  $answer.append(`<h1> <p>${response}</p> </h1>`)
+}
