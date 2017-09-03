eaeb5c3467478648a92c45a139cdda449ed41be8
David Weinstein <dwein9@gmail.com>
2017-09-03 14:47:00 -0400

    can log hr

--- hr-react/src/App.js
+++ hr-react/src/App.js
@@ -4,16 +4,31 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      sayHi: "Hi!",
+      homers: null
     };
   }
+
+  componentDidMount(){
+    const URL = 'https://www.baseball-reference.com/players/gl.fcgi?id=stantmi03&t=b&year=2017'
+
+    var request = new XMLHttpRequest();
+    request.open("GET", URL);
+
+    request.onload = function(){
+      var text = request.responseText
+      console.log( text.split('<tbody>')[1].split('batting_gamelogs.').slice(-1)[0].split('data-stat="HR" >')[1].split('<')[0])
+    };
+    request.send();
+  }
+
 
   render() {
     return (
       <div>
-        {this.state.sayHi}
+        {this.state.homers}
       </div>
     );
   }
 }
+
 export default App;
